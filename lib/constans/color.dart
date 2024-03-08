847356124747b6399e7e8f391c1c110de248c12e
Harry Muliawan <harrymuliawan03@gmail.com>
2024-03-08 18:18:53 +0700

    first commit

---
+++ lib/constans/color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF007BFF);
+  static const Color accent = Color(0xFFffc107);
+  static const Color background = Colors.white;
+}
